e90c138c45734a0f40c3eb85848c544936808cf7
Berat keskin <bk1347444@gmail.com>
2025-01-18 23:20:28 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,38 +1,96 @@
-import pyaudio
-import wave
+import turtle
+import time
+import random
 
-audio = pyaudio.PyAudio()
-stream = audio.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, frames_per_buffer=1024)
-frames = []
+Font = ("arial",35,"normal")
 
-for i in range(0, int(44100 / 1024 * 5)):
-    data = stream.read(1024)
-    frames.append(data)
+screen = turtle.Screen()
+turtle.bgcolor("black")
+text_turtle = turtle.Turtle()
+turtle_turtle = turtle.Turtle()
+counter_runing = True
+point_turtle = turtle.Turtle()
+point = 0
+circle_pos = (0,0)
+turtles =[text_turtle,turtle_turtle,point_turtle]
 
-stream.stop_stream()
-stream.close()
-audio.terminate()
+for color in turtles:
+    color.color("white")
+    color.hideturtle()
 
-wf = wave.open('output.wav', 'wb')
-wf.setnchannels(1)
-wf.setsampwidth(audio.get_sample_size(pyaudio.paInt16))
-wf.setframerate(44100)
-wf.writeframes(b''.join(frames))
-wf.close()
+def update_point_display():
+    point_turtle.clear()
+    point_turtle.write(f"point:{point}",align='center',font=Font)
 
-audio = pyaudio.PyAudio()
-wf = wave.open('output.wav', 'rb')
+def countdown(time):
+    global counter_runing
+    screen.onclick(None)
+    text_turtle.clear()
 
-stream = audio.open(format=audio.get_format_from_width(wf.getsampwidth()),
-                    channels=wf.getnchannels(),
-                    rate=wf.getframerate(),
-                    output=True)
+    if time > 0 :
+        text_turtle.penup()
+        text_turtle.goto(0, 300)
+        text_turtle.pendown()
+        text_turtle.write(time,align='center',font=Font)
+        turtle.ontimer(lambda :countdown(time-1),1000)
+    else:
+        text_turtle.penup()
+        text_turtle.goto(0,300)
+        text_turtle.pendown()
+        text_turtle.write("click screen",align='center',font=Font)
+        screen.onclick(click_screen)
+        counter_runing = False
 
-data = wf.readframes(1024)
-while data:
-    stream.write(data)
-    data = wf.readframes(1024)
+turtle_instance = turtle.Turtle()
+turtle_instance.color("#77B254")
+def circle():
+    global counter_runing
+    turtle_instance.clear()
+    if counter_runing:
+        turtle_instance.penup()
+        x,y = random.randint(-300, 150),random.randint(100,150)
+        turtle_instance.goto(x,y)
+        turtle_instance.pendown()
+        turtle_instance.speed(0)
+        turtle_instance.shape("turtle")
+        turtle_instance.shapesize(2.5)
+        screen.ontimer(circle,500)
 
-stream.stop_stream()
-stream.close()
-audio.terminate()
+def screen_ontimer(x,y):
+   global point
+   if turtle_instance.distance(x,y) < 70:
+        point+=1
+        update_point_display()
+
+def drawing_board_ontimer(x,y):
+    global counter_runing
+    counter_runing=True
+    screen.onclick(None)
+    countdown(30)
+    circle()
+
+def puan_tikla():
+    global point
+    point+=1
+    update_point_display()
+
+def click_screen(x,y):
+    drawing_board_ontimer(x,y)
+    screen_ontimer(x,y)
+    puan_tikla()
+
+def point_screen():
+    point_turtle.penup()
+    point_turtle.goto(0, 240)
+    point_turtle.pendown()
+    point_turtle.speed(0)
+    update_point_display()
+
+point_screen()
+turtle_instance.speed(0)
+screen.onclick(click_screen)
+text_turtle.penup()
+text_turtle.goto(0,300)
+text_turtle.pendown()
+text_turtle.write("Click screen",align="center",font=Font)
+turtle.done()
